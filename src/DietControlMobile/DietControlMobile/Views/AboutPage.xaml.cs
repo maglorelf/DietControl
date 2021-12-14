51b80c0490f14a852136d4c1c705a5265875bc9e
Marcos Javier Martínez Ramos <maglor_elf@yahoo.es>
2021-12-15 00:27:27 +0100

    Template structure #1

---
+++ src/DietControlMobile/DietControlMobile/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DietControlMobile.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/DietControlMobile/DietControlMobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using DietControlMobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace DietControlMobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
